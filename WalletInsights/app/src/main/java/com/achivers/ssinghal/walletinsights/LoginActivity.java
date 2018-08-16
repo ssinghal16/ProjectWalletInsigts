bd409d573c60a7ba0ac95c47f4d18f5798686267
Soniya Singhal <soniyasinghal1991@gmail.com>
2018-08-16 23:14:53 +0530

    WalletInsights Login and Registration Screen
    WalletInsights Login and Registration Screen completed with slider welcome screen and logout option.

---
+++ WalletInsights/app/src/main/java/com/achivers/ssinghal/walletinsights/LoginActivity.java
@@ -0,0 +1,110 @@
+package com.achivers.ssinghal.walletinsights;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonsignin;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignup,textForgetPass;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() !=null){
+            finish();
+           startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+        }
+
+
+        progressDialog= new ProgressDialog(this);
+
+        editTextEmail=(EditText) findViewById(R.id.editTextEmail);
+        editTextPassword=(EditText) findViewById(R.id.editTextPassword);
+        buttonsignin= (Button) findViewById(R.id.buttonSignin);
+        textViewSignup= (TextView) findViewById(R.id.textViewSignup);
+        textForgetPass= (TextView) findViewById(R.id.textForgetPassword);
+
+        buttonsignin.setOnClickListener(this);
+        textViewSignup.setOnClickListener(this);
+        textForgetPass.setOnClickListener(this);
+    }
+
+    private void UserLogin(){
+
+        String email =editTextEmail.getText().toString().trim();
+        String password= editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please Enter email",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please Enter password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Login user Please wait....");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+                if (task.isSuccessful()){
+                   // checkIfEmailVerified();
+                    finish();
+                    //startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                    startActivity(new Intent(getApplicationContext(),WelcomeScreen.class));
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Invalid username or password.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonsignin){
+            UserLogin();
+        }
+        if(view ==textViewSignup){
+            finish();
+            startActivity(new Intent(this,MainActivity.class));
+        }
+        if(view ==textForgetPass){
+
+            startActivity(new Intent(this, ForgotPasswordActivity.class));
+        }
+    }
+
+}
---
+++ WalletInsights/app/src/main/java/com/achivers/ssinghal/walletinsights/WelcomeScreen.java
@@ -0,0 +1,89 @@
+package com.achivers.ssinghal.walletinsights;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    private ViewPager mslideviewPager;
+    private LinearLayout mdotLayout;
+    private SliderAdapter sliderAdapter;
+    private TextView[] mdots;
+    //private RelativeLayout relativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+        mslideviewPager= (ViewPager) findViewById(R.id.viewPager1);
+        mdotLayout= (LinearLayout) findViewById(R.id.mdot);
+      //  relativeLayout= (RelativeLayout) findViewById(R.id.r1);
+        sliderAdapter= new SliderAdapter(this);
+        mslideviewPager.setAdapter(sliderAdapter);
+        mslideviewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+        addDotIndicator(0);
+    }
+
+    public void addDotIndicator(int position){
+        mdots =new TextView[sliderAdapter.getCount()];
+        mdotLayout.removeAllViews();
+
+
+        for(int i=0; i<mdots.length;i++){
+            mdots[i]=new TextView(this);
+            mdots[i].setText(Html.fromHtml("&#8226;"));
+            mdots[i].setTextSize(35);
+            mdots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+
+
+            mdotLayout.addView(mdots[i]);
+        }
+
+        if(mdots.length>0){
+            mdots[position].setTextColor(getResources().getColor(R.color.colorWhite));
+        }
+    }
+
+
+
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @SuppressLint("ClickableViewAccessibility")
+        @Override
+        public void onPageSelected(int i) {
+            addDotIndicator(i);
+            if(i==sliderAdapter.getCount()-1){
+
+                mslideviewPager.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View view, MotionEvent motionEvent) {
+                            Intent i1 =new Intent(WelcomeScreen.this,ProfileActivity.class);
+                            startActivity(i1);
+                            return true;
+                        }
+                    });
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
+}
